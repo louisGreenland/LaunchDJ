f34e7cdefcd183f9eaaf01e1b53c87e4aec8b46a
Louis Greenland <louisgreenland1@hotmail.com>
2023-10-10 16:12:53 +0100

    Added SQLite

--- LaunchDJ-Backend/Program.cs
+++ LaunchDJ-Backend/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
